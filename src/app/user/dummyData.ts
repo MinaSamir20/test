e097f35da378b1176c8058d36f04f3750fc24050
Mina Samir <minasamir9749@gmail.com>
2024-06-04 19:08:20 +0300

    m

---
+++ src/app/user/dummyData.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+  {
+    id: "u1",
+    avatar: "user-1.jpg",
+    name: "Max Schwarz",
+  },
+  {
+    id: "u2",
+    avatar: "user-2.jpg",
+    name: "Manuel Schulz",
+  },
+  {
+    id: "u3",
+    avatar: "user-3.jpg",
+    name: "Julie Schrader",
+  },
+  {
+    id: "u4",
+    avatar: "user-4.jpg",
+    name: "Igor Schwarz",
+  },
+  {
+    id: "u5",
+    avatar: "user-5.jpg",
+    name: "Max Schrader",
+  },
+  {
+    id: "u6",
+    avatar: "user-6.jpg",
+    name: "Julie Schwarz",
+  },
+];
